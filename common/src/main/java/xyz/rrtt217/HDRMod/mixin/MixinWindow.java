e5dcc0ea601a30f41fdd3385d2f87b6569486ced
Zenrap0518 <zenrap518@gmail.com>
2026-06-22 01:06:10 -0700

    Update MixinWindow.java

--- common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinWindow.java
+++ common/src/main/java/xyz/rrtt217/HDRMod/mixin/MixinWindow.java
@@ -71,7 +71,7 @@ import static xyz.rrtt217.HDRMod.HDRMod.configHolder;
             boolean applyWindowsWorkaround = (hasOnlyIntelCard && platform == GLFW.GLFW_PLATFORM_WIN32) && !config.forceDisableGlfwWorkaround;
             if(platform != GLFW.GLFW_PLATFORM_X11 && HDRModMixinPlugin.hasGlfwLib) {
                 // 10 bpc for int
-                if(applyWindowsWorkaround && config.useUNORMWindowPixelFormat) {
+                if(applyWindowsWorkaround || config.useUNORMWindowPixelFormat) {
                     GLFW.glfwWindowHint(GLFW.GLFW_RED_BITS, 10);
                     GLFW.glfwWindowHint(GLFW.GLFW_GREEN_BITS, 10);
                     GLFW.glfwWindowHint(GLFW.GLFW_BLUE_BITS, 10);
